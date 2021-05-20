54d1dc74de237c981ac007e6cfbe80cf37ec9825
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2021-05-20 11:16:23 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pip3 install -r /django/new_chatapp/requirements.txt
+pip3 install -r /django/new_chatapp/fundoo requirements.txt
+
+python3  /django/new_chatapp/fundoo manage.py makemigrations
+
+python3  /django/new_chatapp/fundoo manage.py migrate
+
 
 
 
