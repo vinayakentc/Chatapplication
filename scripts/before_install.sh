d35395cdffa48c326c9f594a005aff4e6301d788
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2021-05-22 12:45:30 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo rm -f requirements.txt
 sudo rm -rf /django/new_chatapp/*
-sudo rm -rf /django/new_chatapp/.github/workflows/pythonapp.yml
+sudo rm -rf /django/new_chatapp/.github/workflows/*
 
 sudo systemctl stop fundoo
